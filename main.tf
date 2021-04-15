806b0f665a1b51bf18758f6a8c92aa71221440ac
McAleer <imcaleer@uk-c02z712slvdm.home>
2021-04-15 21:36:31 +0100

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,106 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-1"
+}
+
+variable "function_name" {default = "mytestlambda"}
+variable "handler" {default = "lambda.lambda_handler"}
+variable "runtime" {default = "python3.8"}
+
+resource "aws_lambda_function" "lambda_function" {
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = var.handler
+  runtime          = var.runtime
+  filename         = "lambda.zip"
+  function_name    = var.function_name
+  source_code_hash = filebase64sha256("lambda.zip")
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_CloudWatchLogs
+  ] 
+}
+
+resource "aws_s3_bucket_notification" "my-trigger" {
+    bucket = "uploads-imca"
+
+    lambda_function {
+        lambda_function_arn = aws_lambda_function.lambda_function.arn
+        events              = ["s3:ObjectCreated:*"]
+        filter_suffix       = ".json"
+    }
+}
+
+resource "aws_lambda_permission" "test" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_function.arn
+  principal = "s3.amazonaws.com"
+  source_arn = "arn:aws:s3:::uploads-imca"
+}
+
+
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": "dynamodb:*",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logging" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_CloudWatchLogs" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_S3ReadOnly" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
