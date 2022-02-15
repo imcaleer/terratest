38480b8e40efda720a04ddabef7239b5b91ed39c
imcaleer <imcaleer@deloitte.co.uk>
2022-02-15 13:09:38 +0000

    changing lambda

--- main.tf
+++ main.tf
@@ -16,13 +16,33 @@ variable "function_name" { default = "mytestlambda" }
 variable "handler" { default = "lambda.lambda_handler" }
 variable "runtime" { default = "python3.8" }
 
+data "archive_file" "zipit" {
+  type        = "zip"
+  source_file = "${path.module}/lambda.py"
+  output_path = "${path.module}/lambda.zip"
+}
+
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = "uploads-imca_lambda"
+}
+
+resource "aws_s3_object" "zipit" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  key    = "lambda.zip"
+  source = data.archive_file.zipit.output_path
+
+  etag = filemd5(data.archive_file.zipit.output_path)
+}
+
 resource "aws_lambda_function" "lambda_function" {
-  role             = aws_iam_role.iam_for_lambda.arn
-  handler          = var.handler
-  runtime          = var.runtime
-  filename         = "lambda.zip"
-  function_name    = var.function_name
-  source_code_hash = filebase64sha256("lambda.zip")
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = var.handler
+  runtime       = var.runtime
+  filename      = "lambda.zip"
+  function_name = var.function_name
+  s3_bucket     = aws_s3_bucket.lambda_bucket
+  s3_key        = "lambda.zip"
 
   depends_on = [
     aws_iam_role_policy_attachment.lambda_CloudWatchLogs
