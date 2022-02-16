fd5b9655918370e74b87ddca66b34984ebd33190
imcaleer <imcaleer@deloitte.co.uk>
2022-02-16 13:58:11 +0000

    changed approach

--- main.tf
+++ main.tf
@@ -16,32 +16,34 @@ variable "function_name" { default = "mytestlambda" }
 variable "handler" { default = "lambda.lambda_handler" }
 variable "runtime" { default = "python3.8" }
 
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = "uploads-imca-lambda-2"
+}
+
 data "archive_file" "zipit" {
   type        = "zip"
   source_file = "${path.module}/lambda.py"
   output_path = "${path.module}/lambda.zip"
 }
 
-resource "aws_s3_bucket" "lambda_bucket" {
-  bucket = "uploads-imca_lambda"
-}
 
-resource "aws_s3_object" "zipit" {
+resource "aws_s3_bucket_object" "zipit" {
   bucket = aws_s3_bucket.lambda_bucket.id
 
   key    = "lambda.zip"
   source = data.archive_file.zipit.output_path
 
   etag = filemd5(data.archive_file.zipit.output_path)
+
+
 }
 
 resource "aws_lambda_function" "lambda_function" {
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = var.handler
   runtime       = var.runtime
-  filename      = "lambda.zip"
   function_name = var.function_name
-  s3_bucket     = aws_s3_bucket.lambda_bucket
+  s3_bucket     = aws_s3_bucket.lambda_bucket.bucket
   s3_key        = "lambda.zip"
 
   depends_on = [
@@ -50,17 +52,21 @@ resource "aws_lambda_function" "lambda_function" {
 }
 
 resource "aws_s3_bucket" "upload_bucket" {
-  bucket = "uploads-imca2"
+  bucket = "imca-source-2"
 }
 
 resource "aws_s3_bucket_notification" "my-trigger" {
-  bucket = aws_s3_bucket.upload_bucket.bucket
+  bucket = aws_s3_bucket.upload_bucket.id
 
   lambda_function {
     lambda_function_arn = aws_lambda_function.lambda_function.arn
     events              = ["s3:ObjectCreated:*"]
     filter_suffix       = ".json"
   }
+
+  depends_on = [
+    aws_lambda_permission.test
+  ]
 }
 
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
@@ -80,11 +86,18 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
 resource "aws_lambda_permission" "test" {
   statement_id  = "AllowS3Invoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda_function.arn
+  function_name = aws_lambda_function.lambda_function.function_name
   principal     = "s3.amazonaws.com"
-  source_arn    = "arn:aws:s3:::uploads-imca2"
+  source_arn    = aws_s3_bucket.upload_bucket.arn
+  qualifier     = aws_lambda_alias.test_alias.name
 }
 
+resource "aws_lambda_alias" "test_alias" {
+  name             = "testalias"
+  description      = "a sample description"
+  function_name    = aws_lambda_function.lambda_function.function_name
+  function_version = "$LATEST"
+}
 
 
 resource "aws_iam_role" "iam_for_lambda" {
